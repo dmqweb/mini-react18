511631030b34fe580af20671ca2a31df9de1b396
dumingqing <dumingqing@xiaomi.com>
2024-08-05 10:16:57 +0800

    'feat:初始化react模块'

---
+++ packages/react/index.js
@@ -0,0 +1,10 @@
+/**
+ * Hooks
+ */
+export { 
+    useReducer,
+    useState,
+    useEffect,
+    useLayoutEffect,
+    __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+ } from './src/React';
---
+++ packages/react/jsx-dev-runtime.js
@@ -0,0 +1,2 @@
+// jsxDev函数,该函数会被@babel/react在转换时自动导入,后续无需在手动导入React.createElement
+export { jsxDEV } from './src/jsx/ReactJSXElement';
